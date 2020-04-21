742eaca6f974b0226bddc4fd9cf20f63cde3645a
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-04-21 20:35:36 -0300

    Lyrics project v.1

--- js/api.js
+++ js/api.js
@@ -14,3 +14,29 @@ export class API{
         }
     }
 }
+
+// import {nombreCliente, ahorro,mostrarInformacion,Cliente/*mostrarNombre*/} from './cliente.js';
+// import{nombreEmpresa, ahorro as ahorroEmpresa,categoria,mostrarInformacion as informacionEmpresa,Empresa} from './Empresa.js'
+
+// /*console.log(nombreEmpresa);
+// console.log(ahorro);
+// console.log(ahorroEmpresa);
+// console.log(categoria);
+// const info=informacionEmpresa(nombreEmpresa,ahorroEmpresa,categoria);
+// console.log(info);*/
+
+// let empresa=new Empresa(nombreEmpresa,ahorroEmpresa,categoria);
+// console.log(empresa.mostrarInformacion());
+// // import * as clientes from './cliente.js';
+
+// //console.log(nombreCliente);
+// //console.log(ahorro);
+
+// /*const info=mostrarInformacion(nombreCliente,ahorro);
+// //console.log(info);
+// */
+// // const mostrarNombres=mostrarNombre(nombreCliente);
+// // console.log(mostrarNombres);
+
+// let cliente=new Cliente(nombreCliente,ahorro);
+// console.log(cliente.mostrarInformacion());
